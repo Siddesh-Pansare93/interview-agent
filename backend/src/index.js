07f077022dd20f996c2129c511b52dc834c5fcbe
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-04-03 00:32:05 +0530

    Add Google GenAI dependency and implement interview question generation endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,15 @@ dotenv.config()
 import express from  'express'
 import cors from 'cors'
 import Vapi from '@vapi-ai/web';
+import {GoogleGenAi} from '@google/genai'
 
 
 //Intialsing express app 
 const app = express()
+
+//Intialise genai
+
+const ai  = await new GoogleGenAi(process.env.GEMINI_API_KEY)
 
 app.use(cors({
     origin : "*"
@@ -14,8 +19,27 @@ app.use(cors({
 
 
 
-app.post("/chat" , ()=>{
-    
+app.post("/generate-interview" ,async (req , res )=>{
+
+    const {role , level , techstack , type , amount } = req.body
+
+
+    const response  =  await ai.models.generateContent({
+        model : "gemini-2.0-flash" , 
+        contents : `Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3
+    `
+    })
 } )
 
 
