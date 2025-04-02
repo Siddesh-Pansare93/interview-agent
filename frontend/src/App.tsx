2285068c7330cf596ab60aa11afc705b585b1730
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-04-03 00:32:16 +0530

    Refactor Vapi initialization and enhance call handling in App component; add Tailwind CSS import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,33 @@ import Vapi from "@vapi-ai/web"
 import { useState ,  useEffect} from "react"
 
 
+
 const App = () => {
 
-  const [vapi , setVapi ] = useState({})
+  // const [vapi , setVapi ] = useState({})
+  const vapi = new Vapi(`${import.meta.env.VITE_VAPI_API_KEY}`)
 
-  useEffect(() => {
-    const vapi = new Vapi(`${process.env.VITE_VAPI_ASSISTANT_TOKEN}`)
-    setVapi(vapi)
-  }, [])
+
+
+
+  const handleStartCall = async ()=>{
+    await vapi.start(import.meta.env.VITE_VAPI_ASSISTANT_TOKEN)
+    console.log("call started")
+
+    vapi.on('call-end' , ()=>{
+      alert("call has been ended")
+    })
+  } 
+
+  const handleEndCall = ()=>{
+    vapi.stop()
+  }
 
   return (
-    <div className="w-screen h-screen bg-black">
-      <div>
-        <button>Start call </button>
-        <button>End Call </button>
+    <div className="w-screen h-screen bg-black flex justify-center items-center ">
+      <div className="space-x-14">
+        <button onClick={handleStartCall}>Start call </button>
+        <button onClick={handleEndCall}>End Call </button>
       </div>
     </div>
   )
