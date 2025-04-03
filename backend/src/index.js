d86a4d54b04d3769f1b2546c40b6393684ef1975
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2025-04-03 21:06:04 +0530

    Enhance error handling and logging in interview question generation endpoint; update Google GenAI initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,19 @@ dotenv.config()
 import express from  'express'
 import cors from 'cors'
 import Vapi from '@vapi-ai/web';
-import {GoogleGenAi} from '@google/genai'
+import { GoogleGenAI } from "@google/genai";
 
 
 //Intialsing express app 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 //Intialise genai
 
-const ai  = await new GoogleGenAi(process.env.GEMINI_API_KEY)
+const ai  =  new GoogleGenAI({apiKey : process.env.GEMINI_API_KEY})
+console.log(process.env.GEMINI_API_KEY)
 
 app.use(cors({
     origin : "*"
@@ -21,25 +25,33 @@ app.use(cors({
 
 app.post("/generate-interview" ,async (req , res )=>{
 
-    const {role , level , techstack , type , amount } = req.body
-
-
-    const response  =  await ai.models.generateContent({
-        model : "gemini-2.0-flash" , 
-        contents : `Prepare questions for a job interview.
-        The job role is ${role}.
-        The job experience level is ${level}.
-        The tech stack used in the job is: ${techstack}.
-        The focus between behavioural and technical questions should lean towards: ${type}.
-        The amount of questions required is: ${amount}.
-        Please return only the questions, without any additional text.
-        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
-        Return the questions formatted like this:
-        ["Question 1", "Question 2", "Question 3"]
-        
-        Thank you! <3
-    `
-    })
+   try {
+     console.log(req.body)
+     const {role , level , techstack , type , amount } = req.body
+ 
+ 
+     const response  =  await ai.models.generateContent({
+         model : "gemini-2.0-flash" , 
+         contents : `Prepare questions for a job interview.
+         The job role is ${role}.
+         The job experience level is ${level}.
+         The tech stack used in the job is: ${techstack}.
+         The focus between behavioural and technical questions should lean towards: ${type}.
+         The amount of questions required is: ${amount}.
+         Please return only the questions, without any additional text.
+         The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+         Return the questions formatted like this:
+         ["Question 1", "Question 2", "Question 3"]
+         
+         Thank you! <3
+     `
+     })
+    //  console.log(response.text);
+      console.log(typeof response.text);
+     res.send(JSON.parse(response.text))
+   } catch (error) {
+        console.log("ERROR :  ", error.message )
+   }
 } )
 
 
